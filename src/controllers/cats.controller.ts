cdc1cc5d32324fe1f1314e88f588d759a5b61d7e
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-04-20 19:11:15 +0530

    added admin can add a cat

--- src/controllers/cats.controller.ts
+++ src/controllers/cats.controller.ts
@@ -6,12 +6,15 @@ import {
   HttpStatus,
   Body,
   Param,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 import { Response } from 'express';
 
 import { CatService } from 'src/services/cats.service';
 
 import { CreateCatDto } from 'src/dto/createCatDto.dto';
+import { AuthGaurd } from 'src/authGuard/auth.guard';
 
 @Controller('cats')
 export class CatsController {
@@ -22,10 +25,22 @@ export class CatsController {
     return await this.catService.getAllCats();
   }
 
+  @UseGuards(AuthGaurd)
   @Post('/new')
-  async addCat(@Res() res: Response, @Body() createCatDto: CreateCatDto) {
-    const newCat = await this.catService.createCat(createCatDto);
-    return res.status(HttpStatus.CREATED).json({ cats: newCat });
+  async addCat(
+    @Res() res: Response,
+    @Body() createCatDto: CreateCatDto,
+    @Request() req,
+  ) {
+    const { user } = req;
+    if (user.isAdmin) {
+      const newCat = await this.catService.createCat(createCatDto);
+      return res.status(HttpStatus.CREATED).json({ cats: newCat, user });
+    } else {
+      return res
+        .status(HttpStatus.UNAUTHORIZED)
+        .json({ error: 'Unauthorized access' });
+    }
   }
 
   @Get('/:id')
--- src/modules/cats.module.ts
+++ src/modules/cats.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 import { CatService } from 'src/services/cats.service';
 
@@ -8,7 +9,13 @@ import { CatsController } from 'src/controllers/cats.controller';
 import { Cats } from 'src/entities/cats.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cats])],
+  imports: [
+    TypeOrmModule.forFeature([Cats]),
+    JwtModule.register({
+      secret: 'jwt_secret',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [CatsController],
   providers: [CatService],
 })
